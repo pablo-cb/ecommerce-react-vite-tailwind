3ca5d81cd0633641d917d32774cd3753c5e79572
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2024-01-16 17:36:36 -0600

    Creating routes for each page of the project & instaling react-router-dom to achieve that

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,5 @@
+import { useRoutes,BrowserRouter } from 'react-router-dom'
+
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -6,16 +8,25 @@ import NotFound from '../NotFound'
 import SignIn from '../SignIn'
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> }, // * = any other
+  ])
+
+  return routes
+}
+
+const App = () => {
+
   return (
-    <div className='bg-red-100'>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
