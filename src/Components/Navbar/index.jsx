9b59331d685a7566ab824e879f5e268bc8c78766
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2024-01-16 18:56:56 -0600

    Styling navbar with tailwind

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,42 +1,75 @@
 import { NavLink } from 'react-router-dom'
 
 const navbar = () => {
+    const activeStyle = 'underline underline-offset-4'
   return (
-    <nav>
+    <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
         {/* Left side */}
-        <ul>
-            <li>
-                <NavLink to='/'>
+        <ul className='flex items-center gap-3'>
+            <li className='font-semibold text-lg'>
+                <NavLink
+                    to='/'
+                >
                     Shopi
                 </NavLink>
             </li>
             <li>
-                <NavLink to='/all'>
+                <NavLink
+                    to='/all'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                }
+                >
                     All
                 </NavLink>
             </li>
             <li>
-                <NavLink to='/clothes'>
+                <NavLink
+                    to='/clothes'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                }
+                >
                     Clothes
                 </NavLink>
             </li>
             <li>
-                <NavLink to='/electronics'>
+                <NavLink
+                    to='/electronics'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                }
+                >
                     Electronics
                 </NavLink>
             </li>
             <li>
-                <NavLink to='/furnitures'>
+                <NavLink
+                    to='/furnitures'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                }
+                >
                     Furnitures
                 </NavLink>
             </li>
             <li>
-                <NavLink to='/toys'>
+                <NavLink
+                    to='/toys'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                    }
+                >
                     Toys
                 </NavLink>
             </li>
             <li>
-                <NavLink to='/others'>
+                <NavLink
+                    to='/others'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                    }
+                >
                     Others
                 </NavLink>
             </li>
@@ -44,22 +77,37 @@ const navbar = () => {
 
 
         {/* Right Side */}
-        <ul>
-            <li>
+        <ul className='flex items-center gap-3'>
+            <li className='text-black/60'>
                 email@email.com
             </li>
             <li>
-                <NavLink to='/my-orders'>
+                <NavLink
+                    to='/my-orders'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                    }
+                >
                     My Orders
                 </NavLink>
             </li>
             <li>
-                <NavLink to='/my-account'>
+                <NavLink
+                    to='/my-account'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                    }
+                >
                     My Account
                 </NavLink>
             </li>
             <li>
-                <NavLink to='/sign-in'>
+                <NavLink
+                    to='/sign-in'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                    }
+                >
                     Sign In
                 </NavLink>
             </li>
